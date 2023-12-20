61844b8c6e9ee036959c5f09a133723f6ebdb460
dwainejade <dwainegnd@gmail.com>
2023-12-19 23:49:19 -0500

    added venus and mercury

--- src/data/planetsData.js
+++ src/data/planetsData.js
@@ -30,7 +30,7 @@ const marsData = {
     axialTilt: 25.19, // in degrees
     rotationPeriod: 24.6635, // in hours
     surfaceTemp: -63, // in Celsius
-    color: "red",
+    color: "#B7410E",
     gravity: 3.72076, // in m/s²
 };
 
@@ -51,10 +51,44 @@ const earthData = {
     gravity: 9.807 // in m/s²
 };
 
+const mercuryData = {
+    name: "Mercury",
+    mass: 3.285e23, // in kilograms
+    radius: 2439.7, // in kilometers
+    orbitalOrigin: sunPosition,
+    orbitalRadius: 57.9e6, // in kilometers
+    orbitalSpeed: 47.87, // in kilometers per second
+    orbitalPeriod: 88, // in Earth days
+    orbitalInclination: 7, // in degrees
+    axialTilt: 0.034, // in degrees (very small)
+    rotationPeriod: 1407.6, // in hours
+    surfaceTemp: 167, // in Celsius (average day)
+    color: "gray",
+    gravity: 3.7 // in m/s²
+};
+
+const venusData = {
+    name: "Venus",
+    mass: 4.867e24, // in kilograms
+    radius: 6051.8, // in kilometers
+    orbitalOrigin: sunPosition,
+    orbitalRadius: 108.2e6, // in kilometers
+    orbitalSpeed: 35.02, // in kilometers per second
+    orbitalPeriod: 225, // in Earth days
+    orbitalInclination: 3.39, // in degrees
+    axialTilt: 177.4, // in degrees (rotates in opposite direction)
+    rotationPeriod: 5832.5, // in hours (retrograde rotation)
+    surfaceTemp: 464, // in Celsius
+    color: "white",
+    gravity: 8.87 // in m/s²
+};
+
 // Alternatively, export all as a single object
 const planetsData = {
+    test: testData,
     Mars: marsData,
     Earth: earthData,
-    test: testData,
+    Mercury: mercuryData,
+    Venus: venusData,
 };
 export default planetsData;

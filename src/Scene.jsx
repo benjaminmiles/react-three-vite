357898ea8debd81a906aa2af679c2bdb6b2b2011
dwainejade <dwainegnd@gmail.com>
2023-12-20 16:04:48 -0500

    adjust sun size and brightness

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -7,17 +7,23 @@ import planetsData from "./data/planetsData";
 
 const Scene = () => {
   const { sunSettings } = useStore();
-  const earthTextures = useTexture({
-    map: "/assets/earth/2k_earth_daymap.jpg",
-    normal: "/assets/earth/2k_earth_normal_map.png",
-    specular: "/assets/earth/2k_earth_specular_map.png",
-  });
+  // const earthTextures = useTexture({
+  //   map: "/assets/earth/2k_earth_daymap.jpg",
+  //   normal: "/assets/earth/2k_earth_normal_map.png",
+  //   specular: "/assets/earth/2k_earth_specular_map.png",
+  // });
+  // const jupiterTextures = useTexture({
+  //   map: "/assets/jupiter/jupiter.png",
+  // });
+  // const venusTextures = useTexture({
+  //   map: "/assets/venus/venus.png",
+  // });
 
   return (
     <>
       <ambientLight intensity={0.2} />
       <pointLight color='#f6f3ea' intensity={1.2} position={[0, 0, 0]} />
-      <Planet bodyData={planetsData.Earth} textures={earthTextures} />
+      <Planet bodyData={planetsData.Earth} />
       <Planet bodyData={planetsData.Moon} />
       <Planet bodyData={planetsData.Mars} />
       <Planet bodyData={planetsData.Venus} />
@@ -26,7 +32,7 @@ const Scene = () => {
       <Planet bodyData={planetsData.Saturn} />
       <Planet bodyData={planetsData.Uranus} />
       <Planet bodyData={planetsData.Neptune} />
-      <Planet bodyData={planetsData.Pluto} />
+      {/* <Planet bodyData={planetsData.Pluto} /> */}
       <Sun position={sunSettings.position} />
       <Stars radius={500} count={5000} factor={20} saturation={0} fade speed={0.5} />
     </>

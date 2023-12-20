850761cc7c9def3e107cae936183b463bfc9832c
dwainejade <dwainegnd@gmail.com>
2023-12-20 16:04:35 -0500

    remove textures

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ import Menu from "./components/Menu";
 const App = () => {
   return (
     <div className='Main'>
-      <Canvas id='Canvas' camera={{ fov: 50, position: [0, 40, 0], near: 0.1, far: 2000 }}>
-        {/* <Perf deepAnalyze /> */}
+      <Canvas id='Canvas' camera={{ fov: 55, position: [0, 40, 0], near: 0.1, far: 2000 }}>
+        <Perf deepAnalyze />
         <OrbitControls minDistance={5} maxDistance={980} zoomSpeed={0.2} dampingFactor={1.2} />
         <Suspense fallback={null}>
           <Scene />
         </Suspense>
         <EffectComposer>
-          <Bloom mipmapBlur intensity={0.2} luminanceThreshold={1} luminanceSmoothing={1.2} radius={0.6} />
+          <Bloom mipmapBlur intensity={0.3} luminanceThreshold={1} luminanceSmoothing={1.2} radius={0.6} />
         </EffectComposer>
       </Canvas>
       <Menu />

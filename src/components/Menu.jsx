3a8fce9593bfa01e276efa533b0ce4cb648f9cbc
dwainejade <dwainegnd@gmail.com>
2023-12-21 14:56:03 -0500

    added more data to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import useStore from "../store/store";
+import useStore, { usePlanetStore } from "../store/store";
 
 const Menu = () => {
   const { simSpeed, setSimSpeed } = useStore();
   const [isOpen, setIsOpen] = useState(false);
+  const { selectedPlanet } = usePlanetStore();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -14,16 +15,36 @@ const Menu = () => {
     setSimSpeed(newSpeed);
   };
 
+  const formatNumber = number => {
+    return new Intl.NumberFormat("en-US", { maximumSignificantDigits: 3 }).format(number);
+  };
+
   return (
     <div className={`menu-con ${isOpen ? "open" : "close"}`}>
       <button onClick={toggleMenu} className='menu-toggle-btn'>
-        menu
+        Menu
       </button>
       <div className='menu-content'>
-        <div className='menu-item'>
-          <label htmlFor='simSpeed'>Simulation Speed: </label>
-          <input id='simSpeed' type='number' min='0' max='400' step='0.1' value={simSpeed} onChange={handleSpeedChange} />
-        </div>
+        {selectedPlanet ? (
+          <div className='planet-details'>
+            <h2>{selectedPlanet.name}</h2>
+            <p>Mass: {formatNumber(selectedPlanet.mass)} kg</p>
+            <p>Radius: {selectedPlanet.radius} km</p>
+            <p>Orbital Radius: {formatNumber(selectedPlanet.orbitalRadius)} km</p>
+            <p>Orbital Speed: {selectedPlanet.orbitalSpeed} km/s</p>
+            <p>Orbital Period: {selectedPlanet.orbitalPeriod} days</p>
+            <p>Orbital Inclination: {selectedPlanet.orbitalInclination} degrees</p>
+            <p>Axial Tilt: {selectedPlanet.axialTilt} degrees</p>
+            <p>Rotation Period: {selectedPlanet.rotationPeriod} hours</p>
+            <p>Surface Temperature: {selectedPlanet.surfaceTemp} °C</p>
+            <p>Gravity: {selectedPlanet.gravity} m/s²</p>
+          </div>
+        ) : (
+          <div className='menu-item'>
+            <label htmlFor='simSpeed'>Simulation Speed: </label>
+            <input id='simSpeed' type='number' min='0' max='400' step='0.1' value={simSpeed} onChange={handleSpeedChange} />
+          </div>
+        )}
       </div>
     </div>
   );
